9965245fa7a9172590d096daa332e87f40991efb
hywang <hywang@netdb.csie.ncku.edu.tw>
2023-12-19 00:34:41 +0800

    update src/page_rank.py

--- src/page_rank.py
+++ src/page_rank.py
@@ -2,14 +2,15 @@ import numpy as np
 from utility.Graph import Graph
 from utility.utils import initialize_graph_from_file
 
-def PageRank_one_iter(graph, d):
+def PageRank_one_iter(graph, d, is_init_page_rank):
     num_nodes = len(graph.nodes)
     T = graph.transition_matrix()  # Transition matrix
     e = np.ones(num_nodes)  # Vector of ones
 
-    # Current PageRank values
-    r = np.array([node.pagerank for node in graph.nodes])
+    if is_init_page_rank:
+        r = np.array([node.pagerank / num_nodes for node in graph.nodes])
     
+    r = np.array([node.pagerank for node in graph.nodes])
     # Calculate the new PageRank values
     new_r = (d / num_nodes) * e + (1 - d) * T.dot(r)
 
@@ -18,8 +19,11 @@ def PageRank_one_iter(graph, d):
         node.pagerank = new_r[i]
 
 def PageRank(graph, d, iteration=100):
-    for _ in range(iteration):
-        PageRank_one_iter(graph, d)
+    for i in range(iteration):
+        is_init_page_rank = False
+        if i == 0:
+            is_init_page_rank = True
+        PageRank_one_iter(graph, d, is_init_page_rank)
 
 
 
@@ -28,7 +32,7 @@ if __name__ == '__main__':
     iteration = 30
     damping_factor = 0.1
 
-    graph = initialize_graph_from_file('./inputs/graph_1.txt')
+    graph = initialize_graph_from_file('./inputs/graph_2.txt')
 
     PageRank(graph, damping_factor)
     print(graph.get_pagerank_list())
